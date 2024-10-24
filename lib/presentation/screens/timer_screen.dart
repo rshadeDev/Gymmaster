ff951d91f6ebdc13dad6c9e2130df3dd044c43a6
Byersen <h.garrido02@ufromail.cl>
2024-10-24 14:23:09 -0300

    creacion vista timer

---
+++ lib/presentation/screens/timer_screen.dart
@@ -0,0 +1,212 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:gymmaster/presentation/screens/home_screen.dart';
+import 'package:gymmaster/presentation/screens/profile_screen.dart';
+
+class timer_screen extends StatefulWidget {
+  @override
+  TimerScreenState createState() => TimerScreenState();
+}
+
+class TimerScreenState extends State<timer_screen> {
+  int timePassed = 0;
+  Timer? _timer;
+  bool isPaused = false;
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  void startTimer() {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (!isPaused) {
+        setState(() {
+          timePassed++;
+        });
+      }
+    });
+  }
+
+  void pauseOrResume() {
+    setState(() {
+      isPaused = !isPaused;
+    });
+  }
+
+  void restartTimer() {
+    setState(() {
+      timePassed = 0;
+      isPaused = false;
+    });
+  }
+
+  String formatTime(int seconds) {
+    int minutes = seconds ~/ 60;
+    int secs = seconds % 60;
+    return minutes.toString().padLeft(2, '0') +
+        ":" +
+        secs.toString().padLeft(2, '0');
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    switch (index) {
+      case 0:
+        break;
+      case 1:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+        );
+        break;
+      case 2:
+        break;
+      case 3:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ProfileScreen()),
+        );
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.close, color: Colors.white),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+            Image.asset(
+              'assets/training_screen/hip_abduction.jpg',
+              height: 200,
+            ),
+            Column(
+              children: [
+                Text(
+                  'Temporizador',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+                Text(
+                  formatTime(timePassed),
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    Text('Series',
+                        style: TextStyle(color: Colors.cyan, fontSize: 18)),
+                    Text('3x',
+                        style: TextStyle(color: Colors.white, fontSize: 18)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text('Repeticiones',
+                        style: TextStyle(color: Colors.cyan, fontSize: 18)),
+                    Text('8 a 10',
+                        style: TextStyle(color: Colors.white, fontSize: 18)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text('Descanso',
+                        style: TextStyle(color: Colors.cyan, fontSize: 18)),
+                    Text('1:10',
+                        style: TextStyle(color: Colors.white, fontSize: 18)),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: pauseOrResume,
+                  child: Text(isPaused ? 'Resume' : 'Pause'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueGrey,
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                  ),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: restartTimer,
+                  child: Text('Restart'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey,
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.black,
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.black,
+          elevation: 0,
+          type: BottomNavigationBarType.fixed,
+          unselectedItemColor: Colors.white70,
+          selectedItemColor: Colors.white,
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.fitness_center),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: '',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
